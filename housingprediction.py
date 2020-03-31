424615316bf9be7a50f9dd449ce424003d007bc2
akung44 <akungxyz@gmail.com>
2020-03-30 20:52:37 -0700

    Processed Data

--- housingprediction.py
+++ housingprediction.py
@@ -1,53 +1,81 @@
 import pandas as pd
 import seaborn as sns
-from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import Lasso
+from sklearn.linear_model import LassoCV
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+import time
+#%matplotlib qt
+
+# Time the code
+start_time = time.time()
 
 # Check columns for numerical/categorical replace with mean/median
 class FillNA:
-    def __init__(self, NA_columns, data):
-        self.na=NA_columns
-        self.data=data
-    def fill_median_na(self):
-        for entries in self.na:
+    def fill_median_na(self, NA_columns, data):
+        for entries in NA_columns:
             try:
-                self.data[entries]=self.data[entries].fillna(np.median(self.data[entries]))   
+                data[entries]=data[entries].fillna(np.median(data[entries]))   
             except TypeError:
-                pass
-    def remove_na_columns(self):
-        for entries in self.na:
+                data=data[data[entries].notna()]
+            return data
+    def remove_na_columns(self, NA_columns, data):
+        for entries in NA_columns:
             try:
-                self.data[entries]=self.data[entries].fillna(np.mean(self.data[entries])) 
+                data[entries]=data[entries].fillna(np.mean(data[entries])) 
             except TypeError:
-                self.data=self.data[self.data[entries].notna()]
-            
-        finaldata=self.data
+                data=data[data[entries].notna()]            
+        finaldata=data
         return finaldata
                         
 class BestFeat:
     def __init__(self, X, y):
         self.X=X
         self.y=y
-    def Params(self, model):
+    def CorrelationMap(self):
+        combined_df=pd.concat([self.X,self.y], axis=1)
+        sns.heatmap(combined_df)
+    def Params_1(self, model, col_names):
         mod=model.fit(self.X, self.y)
-        params=mod.feature_importances_
-        return params
+        coef = pd.Series(mod.feature_importances_, index=col_names)
+        return coef
+    def Params_2(self, model, col_names):
+        mod=model.fit(self.X, self.y)
+        coef = pd.Series(mod.coef_, index=col_names)
+        best_features=coef.sort_values()
+        print("The best score is:", mod.score(self.X, self.y))
+        return best_features
 
+# Under specified NaN percentage keep the column
 def MissingVals(PercentMissing, columns, pct):
     for i in range(len(PercentMissing)):
         if PercentMissing[i][1]<pct:
             pass
         else:
             columns.remove(PercentMissing[i][0])
-            
     return columns
-    
+
+# Obtain columns with numerical values
+def numerical_columns(columns):
+    numerical_entries=pd.DataFrame()
+    for col in columns.columns:
+        try:
+            if columns[col].sum()>=0:
+                numerical_entries=pd.concat([numerical_entries, columns[col]], axis=1)
+            else:
+                pass
+        except TypeError:
+            pass
+    return numerical_entries
+
+# Remove the n largest valued rows for a specific column
+def n_largest_removal(dataframe, column, n):
+    largest_vals=list(dataframe.nlargest(n, column).index)
+    dataframe=dataframe.drop(largest_vals)
+    return dataframe
 
 # Check for columns with NaN vaues
 data=pd.read_csv("train.csv")
@@ -66,8 +94,9 @@ for i in na_checking:
 na_columns=MissingVals(na_missing, na_columns, 0.2)
     
 # Fill numerical values with the average value, drop ID, encode dwelling classification
-removed_na=FillNA(na_columns, data).remove_na_columns()
+removed_na=FillNA().remove_na_columns(na_columns, data)
 removed_na=removed_na.drop(["Id"], axis=1)
+removed_na=removed_na.dropna(axis=1)
 
 initial_encoding_val=65
 numbered_categorical_recording={}
@@ -77,38 +106,89 @@ for i in range(len(removed_na)):
     else:
         numbered_categorical_recording[str(removed_na.iloc[i, 0])]=chr(initial_encoding_val)
         removed_na.iloc[i, 0]=numbered_categorical_recording[str(removed_na.iloc[i, 0])]
-
         initial_encoding_val+=1
-   
-# Feature Selection
-standard_scaling=StandardScaler()
-feature_count=len(removed_na.columns)-1
-X=removed_na.iloc[:, :feature_count]
-y=removed_na.iloc[:, -1]
 
-encoded_features=pd.get_dummies(X)
+# Boxplots to visualize our numerical values for LotArea,GrLivArea remove the 4 highest points
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(30, 30))
+axes[0].boxplot(removed_na["LotArea"])
+axes[1].boxplot(removed_na["GrLivArea"])
+plt.show()
+
+# Remove 4 highest points for Lot Area
+removed_area_outliers=n_largest_removal(removed_na, "LotArea", 4)
+
+# Replot box plots again
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(30, 30))
+axes[0].boxplot(removed_area_outliers["LotArea"])
+axes[1].boxplot(removed_area_outliers["GrLivArea"])
+plt.show()
+
+# Remove the highest points for Group Living Area
+removed_area_outliers=n_largest_removal(removed_area_outliers, "GrLivArea", 4)
+
+# Replot box plots again
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(30, 30))
+axes[0].boxplot(removed_area_outliers["LotArea"])
+axes[1].boxplot(removed_area_outliers["GrLivArea"])
+plt.show()
+
+# Separate true prediction values from the dataset
+y=removed_area_outliers.iloc[:, -1]
+removed_area_outliers=removed_area_outliers.drop(y.name ,axis=1)
+# Record the columns which are numerical
+all_numerical=numerical_columns(removed_area_outliers).columns
+
+# One-hot encode the categorical features
+encoded_features=pd.get_dummies(removed_area_outliers)
 encoded_cols=encoded_features.columns
 
-encoded_features=standard_scaling.fit_transform(encoded_features)
+# Split into training and test sets
+X_train, X_test, y_train, y_test = train_test_split(encoded_features, y, test_size=0.2, random_state=42)
 
-encoded_col_values=[]
-for i in range(len(encoded_features[0])):
-    encoded_col_values.append(encoded_features[:,i])
-    
-encoded_df=pd.DataFrame()
-count=0
-for i in encoded_cols:
-    encoded_df[i]=encoded_col_values[count]
-    count+=1
+# Take the numerical columns and scale, separate from rest of dataset
+numerical_train_col=X_train[all_numerical]
+categorical_train_predictors=X_train.drop(numerical_train_col.columns ,axis=1).reset_index().drop(["index"], axis=1)
+numerical_test_col=X_test[all_numerical]
+categorical_test_predictors=X_test.drop(numerical_test_col.columns, axis=1).reset_index().drop(["index"], axis=1)
 
-encoded_df.columns=encoded_cols
+# Reset indices for Sales Price
+y_train=y_train.reset_index().drop(["index"], axis=1)
+y_test=y_test.reset_index().drop(["index"], axis=1)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Scale the numerical features
+standard_scaling=StandardScaler()
+encoded_features_train=pd.DataFrame(standard_scaling.fit_transform(numerical_train_col), columns=numerical_train_col.columns)
+encoded_features_test=pd.DataFrame(standard_scaling.fit_transform(numerical_test_col), columns=numerical_test_col.columns)
 
-rf=Lasso()
-rf.fit(encoded_df, y)
-len(rf.feature_importances_)
-BestFeat(encoded_df, y).Params(RandomForestRegressor())
+# Merge the one-hot encoded predictors and the standardized predictors
+encoded_features_train=pd.concat([categorical_train_predictors, encoded_features_train] ,axis=1)
+encoded_features_test=pd.concat([encoded_features_test, categorical_test_predictors] ,axis=1)
 
-   
 # Variable Selection
+weighted_col_names=encoded_features_train.columns
+feature_strength=BestFeat(encoded_features_train, y_train).Params_2(LassoCV(cv=5), weighted_col_names)
+largest_absolute_strength=abs(feature_strength)
+
+non_zero_features=largest_absolute_strength[abs(largest_absolute_strength)>5000]
+strongest_features=non_zero_features.sort_values()
+
+# Visualize the features
+index_names=[strongest_features.index[i] for i in range(len(strongest_features))]
+strongest_feature_vals=[strongest_features[i] for i in range(len(strongest_features))]
+plt.figure(figsize=(30,15))
+plt.title("Important Features")
+plt.xticks(rotation='vertical')
+plt.bar(index_names, strongest_feature_vals)
+plt.show()
+
+# Check for high amounts of correlation with a heatmap
+strongest_predictors=encoded_features_train[strongest_features.index]
+correlation_map=pd.concat([strongest_predictors, y_train], axis=1)
+plt.figure(figsize=(30,15))
+sns.heatmap(correlation_map.corr(), annot=True, square=True, annot_kws={"fontsize":4})
+plt.show()
+
+# Choose the largest weighted parameters
+best_train_features=strongest_predictors
+best_test_features=encoded_features_test[strongest_features.index]
+
