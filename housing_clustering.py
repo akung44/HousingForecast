061c25e49963e8467c5d0fc255c2431e5aa18fb5
akung44 <akungxyz@gmail.com>
2020-05-10 14:49:12 -0700

    Changes to preprocessing, K-Means Clustering added

--- housing_clustering.py
+++ housing_clustering.py
@@ -1,76 +1,82 @@
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
-from housingprediction import standardize_full_numerical, y
+from housingprediction import standardize_full_numerical, standardize_best_features,y
 import pandas as pd
 from sklearn.decomposition import PCA
 import seaborn as sns
+import numpy as np
 
 # Fit the PCA model
-pca=PCA(n_components=20)
-components=pca.fit_transform(standardize_full_numerical)
+pca=PCA(n_components=9)
+components=pca.fit_transform(standardize_best_features)
 feature_count=range(pca.n_components_)
 pca_var=pca.explained_variance_ratio_.tolist()
 
-# Check the variance ratio for each principle component
+# Check the variance ratio for each principal component
+plt.figure(figsize=(15,10))
 plt.bar(feature_count, pca_var, color="blue")
-
-# Visualize the components to see if there are any clusters
-pca_clusters=pd.DataFrame(components)
-
-plt.figure(figsize=(20,10))
-plots = []
-count=0
-for i in range(3):
-    for j in range(4):
-        if j>i:
-            count+=1
-            if count <=3:
-                ax = plt.subplot2grid((5,4), (1,count))
-                ax.scatter(pca_clusters[i], pca_clusters[j])
-            else:
-                ax = plt.subplot2grid((5,4), (2,count-3))
-                ax.scatter(pca_clusters[i], pca_clusters[j])
+plt.title("PCA Explained Variance by component")
+plt.ylabel("Variance Explained")
+plt.xlabel("Principal Component Number")
 plt.show()
 
-# Minimize inertia by evaluating KMeans at various k values
+# Choose the first 4 components
+pca_clusters=pd.DataFrame(components[:, :4])
+
+# Visualize the components to see if there are any clusters
+fig, axes=plt.subplots(3, 2, figsize=(15,15))
+axes[0, 0].scatter(pca_clusters.iloc[:,0], pca_clusters.iloc[:, 1])
+axes[0, 0].set_title('PC 2 vs PC 1')
+axes[0, 1].scatter(pca_clusters.iloc[:,0], pca_clusters.iloc[:, 2])
+axes[0, 1].set_title('PC 3 vs PC 1')
+axes[1, 0].scatter(pca_clusters.iloc[:,0], pca_clusters.iloc[:, 3])
+axes[1, 0].set_title('PC 4 vs PC 1')
+axes[1, 1].scatter(pca_clusters.iloc[:,1], pca_clusters.iloc[:, 2])
+axes[1, 1].set_title('PC 3 vs PC 2')
+axes[2, 0].scatter(pca_clusters.iloc[:,1], pca_clusters.iloc[:, 3])
+axes[2, 0].set_title('PC 4 vs PC 2')
+axes[2, 1].scatter(pca_clusters.iloc[:,2], pca_clusters.iloc[:, 3])
+axes[2, 1].set_title('PC 4 vs PC 3')
+
+# Minimize SSE by evaluating KMeans at various k values
 cluster_inertia=[]
-for i in range(1,11):
+for i in range(1,21):
     k_clusters=KMeans(n_clusters=i, init='k-means++', max_iter=1000, n_init=20, random_state=42)
-    k_clusters.fit(pca_clusters.iloc[:, :4])
+    k_clusters.fit(pca_clusters)
     cluster_inertia.append(k_clusters.inertia_)
 
-# Visualize the inertia function over clusters    
-plt.plot(range(1,11), cluster_inertia)
+# Visualize the inertia function over clusters  
+plt.figure(figsize=(15,10))
+plt.plot(range(1,21), cluster_inertia)
 plt.title("Inertia per cluster")
+plt.xticks(np.arange(1, 21, 1))
 plt.xlabel("Cluster")
 plt.ylabel("Inertia")
 plt.show()
 
-# Delta between increasing the cluster size by 1
+# Difference between increasing the cluster size by 1
 delta_inertia=[(cluster_inertia[i-1]-cluster_inertia[i]) for i in range(1,len(cluster_inertia))]
+plt.figure(figsize=(15,10))
 plt.plot(range(1,len(delta_inertia)+1), delta_inertia)
 plt.title("Change in Inertia per cluster added")
-plt.xlabel("Cluster k+1")
+plt.xticks(np.arange(1, 21, 1))
+plt.xlabel("Previous Cluster k")
 plt.ylabel("Decline by adding cluster")
 plt.show()
 
 # Choose the value where the decrease in inertia declines
-k_means=KMeans(n_clusters=4, init='k-means++', max_iter=1000, n_init=20, random_state=42)
-k_means.fit(pca_clusters.iloc[:, :4])
-groupings=k_means.predict(pca_clusters.iloc[:, :4])
+k_means=KMeans(n_clusters=6, init='k-means++', max_iter=1000, n_init=20, random_state=42)
+k_means.fit(pca_clusters)
+groupings=k_means.predict(pca_clusters)
 centers=k_means.cluster_centers_
 
-# Predicted clusters
-pca_clusters["Cluster"]=k_means.labels_
-cluster_centers=pd.DataFrame(centers, columns=["C1", "C2", "C3", "C4"])
-
 # Choose k clusters
-clusters=pca_clusters.iloc[:, :4]
+clusters=pca_clusters
 clusters["Cluster"]=k_means.labels_
 
 # Graph the clusters
-colors=["red", "blue", "green"]
-sns.pairplot(clusters, hue="Cluster", palette="Dark2", diag_kind="kde" ,size=1.85)
+colors=["red", "blue", "green", "orange", "violet", "black", "yellow"]
+sns.pairplot(clusters, hue="Cluster", palette="Dark2", diag_kind="kde" , height=1.85)
 plt.show()
 
 
--- housingprediction.py
+++ housingprediction.py
@@ -3,15 +3,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.linear_model import LassoCV
+from sklearn.ensemble import RandomForestRegressor
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
 from sklearn.preprocessing import StandardScaler, normalize
+from sklearn.impute import SimpleImputer, KNNImputer
 from sklearn.model_selection import train_test_split
-import time
-#%matplotlib qt
-
-# Time the code
-start_time = time.time()
 
 # Check columns for numerical/categorical replace with mean/median
 class FillNA:
@@ -27,27 +24,24 @@ class FillNA:
             try:
                 data[entries]=data[entries].fillna(np.mean(data[entries])) 
             except TypeError:
-                data=data[data[entries].notna()]            
+                imp_mean = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+                imp_mean.fit_transform(data[entries])
+                data[entries]=data[data[entries].notna()]            
         finaldata=data
         return finaldata
                         
 class BestFeat:
-    def __init__(self, X, y):
-        self.X=X
-        self.y=y
-    def CorrelationMap(self):
-        combined_df=pd.concat([self.X,self.y], axis=1)
-        sns.heatmap(combined_df)
-    def Params_1(self, model, col_names):
-        mod=model.fit(self.X, self.y)
-        coef = pd.Series(mod.feature_importances_, index=col_names)
-        return coef
-    def Params_2(self, model, col_names):
-        mod=model.fit(self.X, self.y)
+    def Params(self, predictors, output, model, col_names):
+        mod=model.fit(predictors, output)
         coef = pd.Series(mod.coef_, index=col_names)
         best_features=coef.sort_values()
-        print("The best score is:", mod.score(self.X, self.y))
         return best_features
+    def Trees(self, predictors, output, model, col_names):
+        mod=model.fit(predictors, output)
+        coef = pd.Series(mod.feature_importances_, index=col_names)
+        best_features=coef.sort_values()
+        return best_features
+
 
 # Under specified NaN percentage keep the column
 def MissingVals(PercentMissing, columns, pct):
@@ -79,25 +73,66 @@ def n_largest_removal(dataframe, column, n):
 
 # Check for columns with NaN vaues
 data=pd.read_csv("train.csv")
-
+imputed_data=data.copy()
+# Search for columns with NA and give percentage of columns NA
 na_checking=(data.isna().sum())/len(data)
 na_missing=[]
 na_columns=[]
 
 count=0
+# Separate all the NA columns from columns with no missing values
 for i in na_checking:
     if i>0:
         na_missing.append((data.columns[count], i))
         na_columns.append(data.columns[count])
     count+=1
-    
+
+# Remove columns with greater than 20% of values missing
 na_columns=MissingVals(na_missing, na_columns, 0.2)
-    
+# Separate categorical from numerical columns
+categorical_only=[]
+numerical_only=na_columns.copy()
+for i in na_columns:
+    if type(data[i][0])==str:
+        categorical_only.append(i)
+        numerical_only.remove(i)
+
+# Count the available values for each categorical column of NaN values
+categorical_entry_count=[data[cols].value_counts() for cols in categorical_only]
+# Impute categorical nan with most frequent category if high amount, otherwise randomly choose
+imp_freq = SimpleImputer(missing_values=np.nan, strategy='most_frequent')
+for i,j in enumerate(categorical_entry_count):
+    cat_col_name=categorical_only[i]
+    ratio_majority=j[0]/sum(j)
+    if ratio_majority>0.66:
+        cat_col=data[cat_col_name].values.reshape(-1,1)
+        most_freq=imp_freq.fit_transform(cat_col)
+        imputed_data[cat_col_name]=most_freq
+    else:
+        choices=j.index
+        prob=[k/sum(categorical_entry_count[i]) for k in categorical_entry_count[i]]
+        imputed_data[cat_col_name]=data[cat_col_name].apply(lambda x: np.random.choice(choices, p=prob) if pd.isnull(x) else x)
+
+# Retrieve variables most correlated to the columns with missing values
+numerical_corr=[data.corr()[i].drop(index='SalePrice').sort_values(ascending=False).apply(lambda x: x if (x < 1 and x>0.3) else np.nan) for i in numerical_only]
+numerical_corr=[i.dropna() for i in numerical_corr]
+# Standardize data to apply KNN and impute values
+standardize_imputer=StandardScaler()
+# Impute missing numerical values by applying KNearest Neighbors
+knn_impute=KNNImputer(missing_values=np.nan, n_neighbors=5)
+for i in numerical_corr:
+    imputed_col_name=i.name
+    columns=i.index.append(pd.Index([imputed_col_name]))
+    standardized_numerical_na=standardize_imputer.fit_transform(data[columns])
+    tmp=knn_impute.fit_transform(standardized_numerical_na)
+    transformed=standardize_imputer.inverse_transform(tmp)
+    imputed_data[imputed_col_name]=transformed[:,transformed.shape[1]-1]
+     
 # Fill numerical values with the average value, drop ID, encode dwelling classification
-removed_na=FillNA().remove_na_columns(na_columns, data)
-removed_na=removed_na.drop(["Id"], axis=1)
+removed_na=imputed_data.drop(["Id"], axis=1)
 removed_na=removed_na.dropna(axis=1)
 
+# Convert MSubClass into categorical, rather than keeping in numerical form
 initial_encoding_val=65
 numbered_categorical_recording={}
 for i in range(len(removed_na)):
@@ -112,6 +147,8 @@ for i in range(len(removed_na)):
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(30, 30))
 axes[0].boxplot(removed_na["LotArea"])
 axes[1].boxplot(removed_na["GrLivArea"])
+axes[0].title.set_text("Lot Area")
+axes[1].title.set_text("GrLivArea")
 plt.show()
 
 # Remove 4 highest points for Lot Area
@@ -121,6 +158,8 @@ removed_area_outliers=n_largest_removal(removed_na, "LotArea", 4)
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(30, 30))
 axes[0].boxplot(removed_area_outliers["LotArea"])
 axes[1].boxplot(removed_area_outliers["GrLivArea"])
+axes[0].title.set_text("Lot Area")
+axes[1].title.set_text("GrLivArea")
 plt.show()
 
 # Remove the highest points for Group Living Area
@@ -129,11 +168,17 @@ removed_area_outliers=n_largest_removal(removed_area_outliers, "GrLivArea", 4)
 # Replot box plots again
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(30, 30))
 axes[0].boxplot(removed_area_outliers["LotArea"])
+axes[0].title.set_text("Lot Area")
 axes[1].boxplot(removed_area_outliers["GrLivArea"])
+axes[1].title.set_text("GrLivArea")
 plt.show()
 
+# Combine columns for possible predictors
+removed_area_outliers['Bathrooms']=removed_area_outliers['BsmtFullBath'] + removed_area_outliers['FullBath'] + 0.5*removed_area_outliers['BsmtHalfBath'] +  0.5*removed_area_outliers['HalfBath']
+removed_area_outliers['TotalInsideArea']=removed_area_outliers['GrLivArea'] + removed_area_outliers['TotalBsmtSF']
+
 # Separate true prediction values from the dataset
-y=removed_area_outliers.iloc[:, -1]
+y=removed_area_outliers.loc[:, 'SalePrice']
 removed_area_outliers=removed_area_outliers.drop(y.name ,axis=1)
 # Record the columns which are numerical
 all_numerical=numerical_columns(removed_area_outliers).columns
@@ -145,54 +190,60 @@ encoded_cols=encoded_features.columns
 # Standardize dataset for PCA and K-Means
 full_categorical=encoded_features.drop(all_numerical ,axis=1).reset_index().drop(["index"], axis=1)
 standardize_full_numerical=pd.DataFrame(StandardScaler().fit_transform(encoded_features[all_numerical]), columns=all_numerical)
-standardize_full_numerical=pd.concat([standardize_full_numerical, full_categorical], axis=1)
+standardize_full_categorical=pd.DataFrame(StandardScaler().fit_transform(full_categorical), columns=full_categorical.columns)
+standardize_full=pd.concat([standardize_full_numerical, standardize_full_categorical], axis=1)
 
 # Split into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(encoded_features, y, test_size=0.2, random_state=42)
 
 # Take the numerical columns and scale, separate from rest of dataset
 numerical_train_col=X_train[all_numerical]
-categorical_train_predictors=X_train.drop(numerical_train_col.columns ,axis=1).reset_index().drop(["index"], axis=1)
+categorical_train_predictors=X_train.drop(numerical_train_col.columns ,axis=1)
 numerical_test_col=X_test[all_numerical]
-categorical_test_predictors=X_test.drop(numerical_test_col.columns, axis=1).reset_index().drop(["index"], axis=1)
+categorical_test_predictors=X_test.drop(numerical_test_col.columns, axis=1)
 
-# Reset indices for Sales Price
-y_train=y_train.reset_index().drop(["index"], axis=1)
-y_test=y_test.reset_index().drop(["index"], axis=1)
+# Merge the one-hot encoded predictors and the numerical predictors
+complete_column_names=categorical_train_predictors.columns.append(numerical_train_col.columns)
+features_train=pd.concat([categorical_train_predictors, numerical_train_col], axis=1).reset_index().drop(['index'], axis=1)
+features_test=pd.concat([categorical_test_predictors, numerical_test_col], axis=1).reset_index().drop(['index'], axis=1)
 
 # Scale the numerical features
 standard_scaling=StandardScaler()
-encoded_features_train=pd.DataFrame(standard_scaling.fit_transform(numerical_train_col), columns=numerical_train_col.columns)
-encoded_features_test=pd.DataFrame(standard_scaling.fit_transform(numerical_test_col), columns=numerical_test_col.columns)
+encoded_features_train=pd.DataFrame(standard_scaling.fit_transform(features_train), columns=complete_column_names)
+encoded_features_test=pd.DataFrame(standard_scaling.fit_transform(features_test), columns=complete_column_names)
 
-# Merge the one-hot encoded predictors and the standardized predictors
-encoded_features_train=pd.concat([categorical_train_predictors, encoded_features_train] ,axis=1)
-encoded_features_test=pd.concat([encoded_features_test, categorical_test_predictors] ,axis=1)
-
-# Variable Selection
+# Variable Selection with Lasso
 weighted_col_names=encoded_features_train.columns
-feature_strength=BestFeat(encoded_features_train, y_train).Params_2(LassoCV(cv=5), weighted_col_names)
+feature_strength=BestFeat().Params(encoded_features_train, y_train ,LassoCV(cv=5), weighted_col_names)
 largest_absolute_strength=abs(feature_strength)
 
-non_zero_features=largest_absolute_strength[abs(largest_absolute_strength)>7500]
-strongest_features=non_zero_features.sort_values()
+# Variable Selection with Random Forest
+tree_features=BestFeat().Trees(encoded_features_train, y_train.values.ravel(), RandomForestRegressor(n_estimators=100), weighted_col_names)
+largest_tree_strength=abs(feature_strength)
+
+significant_lasso_features=largest_absolute_strength[abs(largest_absolute_strength)> 5000]
+significant_tree_features=largest_tree_strength[abs(largest_tree_strength) > 5000]
+strongest_features=significant_lasso_features.sort_values()
+strongest_features_trees=significant_tree_features.sort_values()
 
 # Visualize the features
-index_names=[strongest_features.index[i] for i in range(len(strongest_features))]
-strongest_feature_vals=[strongest_features[i] for i in range(len(strongest_features))]
-plt.figure(figsize=(30,15))
-plt.title("Important Features")
-plt.xticks(rotation='vertical')
-plt.bar(index_names, strongest_feature_vals)
+tree_index_names=[strongest_features_trees.index[i] for i in range(len(strongest_features_trees))]
+lasso_index_names=[strongest_features.index[i] for i in range(len(strongest_features))]
+strongest_lasso_vals=[strongest_features[i] for i in strongest_features.index]
+strongest_tree_vals=[strongest_features_trees[i] for i in strongest_features_trees.index]
+
+fig, feat = plt.subplots(nrows=1, ncols=2, figsize=(30, 30))
+feat[0].bar(lasso_index_names, strongest_lasso_vals)
+feat[0].title.set_text("Lasso Parameters")
+feat[1].bar(tree_index_names ,strongest_tree_vals)
+feat[1].title.set_text("Random Forest Parameters")
+plt.setp(feat[0].get_xticklabels(), rotation=90)
+plt.setp(feat[1].get_xticklabels(), rotation=90)
 plt.show()
 
-# Check for high amounts of correlation with a heatmap
-strongest_predictors=encoded_features_train[strongest_features.index]
-correlation_map=pd.concat([strongest_predictors, y_train], axis=1)
-plt.figure(figsize=(30,15))
-sns.heatmap(correlation_map.corr(), annot=True, square=True, annot_kws={"fontsize":4})
-plt.show()
+# Unscaled best training and test features
+strongest_predictors=features_train[strongest_features.index]
+unscaled_test=features_test[strongest_features.index]
 
-# Choose the largest weighted parameters
-best_train_features=strongest_predictors
-best_test_features=encoded_features_test[strongest_features.index]
+# Most important features that are standardized to be used for visualization
+standardize_best_features=standardize_full[strongest_predictors.columns]
